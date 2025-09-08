d9be911d925720e14af1dc9c6db123cb100d7066
AkramD0224015 <akramsaja37@gmail.com>
2025-09-08 14:01:48 +0800

    day1.java
     Program "Hello World" dan struktur dasar kode Java

--- day1.java
+++ day1.java
@@ -1,11 +1,7 @@
-package akram;//deklarasi package
-public class latihan1 {//untuk menyimpan code
-// Membuat class latihan1
-        public static void main(String[] args) {//method utama
-            String nama = "Akram";//deklarasi variabel bertipe string
-            int umur = 18;//deklarasi variabel bertipe integer
-            System.out.println("Hello, world!");//menampilkan teks "hello word" ke layar
-            System.out.println("Nama saya: " + nama);//menampilkan teks"nama saya:"lalu di gabung dengan isi variabel nama
-            System.out.println("Umur saya: " + umur + " tahun");//menampilkan teks "umur saya:lalu di gabung dengan isi variabel umur + teks "tahun"
+package akram;
+public class latihan1{
+        public static void main(String[] args) {
+            System.out.println("Hello, world!");
+   
         }
-    }
+}
