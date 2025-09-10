784a9ee9a655aade0d8492b05f07c137f339c7f0
AkramD0224015 <akramsaja37@gmail.com>
2025-09-10 18:57:57 +0800

    day3.java
    Memahami dan menggunakan Komentar

---
+++ day3.java
@@ -0,0 +1,23 @@
+package akram;
+
+public class day3 {
+
+    public static void main(String[] args) {
+        // Digunakan untuk memberikan penjelasan satu baris kode
+        System.out.println("komentar satu baris");
+
+        /*
+        digunakan untuk memberikan penjelasan
+         panjang atau lebih dari satu baris kode
+        */
+        System.out.println("lebih dari satu baris");
+
+        /**
+         * Komentar dokumentasi:
+         *  biasa di gunakan untuk Menjelaskan class, atau method
+         */
+        System.out.println("Selalu diawali /** dan diakhiri */");
+    }
+
+    }
+
