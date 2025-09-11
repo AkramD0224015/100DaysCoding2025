bd4178536cf1a593a72bb9a0af484e93da4c8152
Akram <akramsaja37@gmail.com>
2025-09-11 18:16:13 +0800

    day4.java
    Penggunaan Escape Sequence (\n, \t, dan lain lain).

---
+++ day4.java
@@ -0,0 +1,12 @@
+package akram;
+
+public class day4 {
+        public static void main(String[] args) {
+            System.out.println("Data Mahasiswa:\nNama: Akram\nNIM: D0224015");// \n membuat baris baru
+            System.out.println("Mata Kuliah\tSKS\tNilai");                 // \t memberikan tabulasi
+            System.out.println("Keterangan: \"Mahasiswa biasa\"");          // \" menampilkan tanda kutip ganda
+            System.out.println("file disimpan: C:\\Users\\Akram\\Dokumen"); // \\ menampilkan backslash
+        }
+    }
+
+
