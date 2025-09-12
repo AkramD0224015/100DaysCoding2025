2776e6b29d97325d77c31b7cf59c0496c851d06a
Akram <akramsaja37@gmail.com>
2025-09-12 19:48:40 +0800

    day5.java
    Tipe Data Numerik Bilangan Bulat (byte, short, int, long).

---
+++ day5.java
@@ -0,0 +1,18 @@
+package akram;
+
+public class day5 {
+    public static void main(String[] args) {
+
+        byte umur = 19;
+        short uangjajan = 5000;
+        int gaji = 2500000;
+        long uangsimpanan = 50000000L;
+
+        System.out.println("umur freya" + umur);
+        System.out.println("uang jajan freya: " + uangjajan);
+        System.out.println("gaji freya sebulan: " + gaji);
+        System.out.println("uang simpanan freya: " + uangsimpanan);
+    }
+}
+
+
