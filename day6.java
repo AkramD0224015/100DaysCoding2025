b542abf70c7f541af287965e2b8f5f94cc18b04a
Akram <akramsaja37@gmail.com>
2025-09-13 20:26:16 +0800

    day6.java
    tipe Data Pecahan (double, float)

---
+++ day6.java
@@ -0,0 +1,14 @@
+package akram;
+
+public class day6 {
+    public static void main(String[] args) {
+            double rrqvsevos= 2.0;
+            float penjualantiket = 150.0f;
+
+            System.out.println("scor derbi klasik hari ini="+rrqvsevos);
+            System.out.println("penjualan tiket sore ini="+penjualantiket);
+        }
+    }
+
+
+
