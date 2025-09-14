3428416188552934df632ea1e381a7226a86def1
Akram <akramsaja37@gmail.com>
2025-09-14 18:52:50 +0800

    day7.java
    Tipe Data Karakter (char) dan Boolean (boolean).

---
+++ day7.java
@@ -0,0 +1,10 @@
+package akram;
+
+public class day7 {
+    public static void main(String[] args) {
+        boolean hujan = true;
+        char inisial = 'A';
+        System.out.println("apakah hari ini sedang hujan:"+ hujan );
+        System.out.println("inisial nama saya:" + inisial );
+    }
+}
