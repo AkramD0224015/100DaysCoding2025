c059e9e7e26286617d6d87eca283fa62cd7f9786
Akram <akramsaja37@gmail.com>
2025-09-15 07:06:08 +0800

    day8.java
    Pengenalan Tipe Data String untuk teks.

---
+++ day8.java
@@ -0,0 +1,11 @@
+package akram;
+
+public class day8 {
+    public static void main(String[] args) {
+        String hari = "senin";
+        String bulan = "september";
+
+        System.out.println("sekarang hari apa: "+hari);
+        System.out.println("sekarang bulan apa: " + bulan);
+    }
+}
