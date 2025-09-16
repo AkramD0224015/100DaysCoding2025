84324cc201af42340f891d8fc2b009790816f659
Akram <akramsaja37@gmail.com>
2025-09-16 21:57:26 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,12 @@
+package akram;
+
+public class day9 {
+    public static void main(String[] args) {
+        int angka; 
+        angka = 10; 
+        System.out.println("Nilai awal: " + angka);
+
+        angka = 15; 
+        System.out.println("setelah di update: " + angka);
+    }
+}
