6a648b8a52a37500bfcf6264c1ce58312581388e
Akram <akramsaja37@gmail.com>
2025-09-17 21:13:23 +0800

    day10.java
    Constanta (Final Variabel).

---
+++ day10.java
@@ -0,0 +1,10 @@
+package akram;
+
+public class day10 {
+    public static void main(String[] args) {
+        // nilainya tetap tidak bisa di ubah
+        final int tanggal = 17;
+        System.out.println("sekarang tanggal : "+tanggal);
+    }
+}
+
