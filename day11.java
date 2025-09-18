a5d89f1a024824603fa071d1ad6b71c4ba88cf9b
Akram <akramsaja37@gmail.com>
2025-09-18 22:55:44 +0800

    day11.java
    Menerima Input dari Keyboard (pengenalan Scanner)

---
+++ day11.java
@@ -0,0 +1,14 @@
+package akram;
+import java.util.Scanner;
+
+public class day11 {
+
+    public static void main(String[] args) {
+        Scanner akr = new Scanner(System.in);
+        System.out.print("Apakah kamu suka musik (true/false): ");
+        boolean suka = akr.nextBoolean();
+        System.out.println("Jawaban: " + suka);
+
+
+    }
+}
