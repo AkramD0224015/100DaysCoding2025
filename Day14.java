de70c8fcc6c858fb25c9b8a755feb00a3135205f
Akram <akramsaja37@gmail.com>
2025-09-21 21:38:52 +0800

    Day14.java
    Mengubah String menjadi Tipe Data Primitif

---
+++ Day14.java
@@ -0,0 +1,22 @@
+package akram;
+
+public class day14 {
+    public static void main(String[] args) {
+       String s1= "40";
+       String s2="50.5";
+       String s3 ="true";
+       
+       int i1 =
+       Integer.parseInt(s1);
+       double d2 =
+       Double.parseDouble(s2);
+       boolean b3 =
+       Boolean.parseBoolean(s3);
+       
+       System.out.println("Int      :"+i1);
+       System.out.println("Double   :"+d2);
+       System.out.println("Boolean  :"+b3);
+       
+    }
+       
+}
