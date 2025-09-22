d5ed15fca768e656090d422ec85904cacf7a2bb7
Akram <akramsaja37@gmail.com>
2025-09-23 01:21:50 +0800

    day16.java
    Operator Aritmatika Perkalian (*) dan Pembagian (/).

---
+++ day16.java
@@ -0,0 +1,17 @@
+package akram;
+
+public class day16 {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2= 2;
+        int perkalian =angka1 * angka2;
+        int pembagian = angka1 / angka2;
+
+        System.out.println(
+                "Hasil Perkalian : " + perkalian +
+                 "\nHasil Pembagian : " + pembagian
+        );
+
+    }
+}
+
