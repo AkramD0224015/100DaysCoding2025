805bdba6d64deed8b3e6dec13f9bc9b1628f6f28
Akram <akramsaja37@gmail.com>
2025-09-22 20:41:14 +0800

    day15.java
    Operator Aritmatika Penjumlahan (+) dan Pengurangan (-)

---
+++ day15.java
@@ -0,0 +1,13 @@
+package akram;
+
+public class day15 {
+    public static void main(String[] args) {
+        int angka1 = 51;
+        int angka2= 49;
+
+        System.out.println("penjumlahan :"+(angka1+angka2));
+        System.out.println("pengurangan :"+(angka1-angka2));
+
+
+    }
+}
