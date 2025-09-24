2edfbeb38e026759f2ea7d84412be9efaec4ce34
Akram <akramsaja37@gmail.com>
2025-09-24 20:52:24 +0800

    day17.java

---
+++ day17.java
@@ -0,0 +1,9 @@
+public class day17 {
+    public static void main(String[] args) {
+        int angka1 = 59;
+        int angka2 = 5;
+        int sisabagi= angka1 % angka2;
+
+        System.out.println("sisa baginya adalah: "+ sisabagi);
+    }
+}
