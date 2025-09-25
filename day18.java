1ca691a38a2970e614a70f400dd4906d63826030
Akram <akramsaja37@gmail.com>
2025-09-25 22:13:23 +0800

    Create day18.java
    Konversi Otomatis (Dari Tipe Data Kecil ke Besar).

---
+++ day18.java
@@ -0,0 +1,14 @@
+public class day18 {
+    public static void main(String[] args) {
+        byte b = 100;
+        int i=b;
+        long l=i;
+        double d =l;
+
+        System.out.println("nilai byte              : "+b);
+        System.out.println("konversi byte ke int    : "+i);
+        System.out.println("konversi int ke long    : "+l);
+        System.out.println("konversi long ke double : "+d);
+
+    }
+}
