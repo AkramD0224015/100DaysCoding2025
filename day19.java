f75ac54d0b5cb6b92f9aa00982a27706964c93ba
Akram <akramsaja37@gmail.com>
2025-09-26 21:57:35 +0800

    day19.java
    Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).

---
+++ day19.java
@@ -0,0 +1,12 @@
+public class day19 {
+    public static void main(String[] args) {
+        double D = 145.54;
+        long L = (long) D;
+        int I = (int) L;
+
+        System.out.println("nilai double            : " + D);
+        System.out.println("konversi double ke long : " + L);
+        System.out.println("konversi long ke int    : " + I);
+
+    }
+}
