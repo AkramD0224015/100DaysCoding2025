04a7bf917c4c0dec02a72582acd692fe82ec930d
Akram <akramsaja37@gmail.com>
2025-09-27 16:00:45 +0800

    day20.java
    Mengubah Tipe Data Primitif menjadi String.

---
+++ day20.java
@@ -0,0 +1,15 @@
+public class day20 {
+    public static void main(String[] args) {
+            int a = 300;
+            double b = 3.4;
+            boolean bool = true;
+
+            String strint = String.valueOf(a);
+            String strdouble = String.valueOf(b);
+            String strboolean = String.valueOf(bool);
+
+        System.out.println("int ke String     : "+ strint);
+        System.out.println("double ke string  : "+ strdouble);
+        System.out.println("boolean ke string : "+ strboolean);
+        }
+    }
