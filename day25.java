2e0c71e7927c5a9570f9a9394cb799561b67ff1e
Akram <akramsaja37@gmail.com>
2025-10-02 22:57:48 +0800

    day25.java
    Operator Increment dan Decrement (++, --)

---
+++ day25.java
@@ -0,0 +1,16 @@
+package akram;
+
+public class day25 {
+    public static void main(String[] args) {
+        //increment  
+        for (int i = 1; i <= 9 ; i++) {
+            System.out.println("nilai :"+i);
+        }
+        //decrement
+        System.out.println("");
+        for (int i = 9; i >= 1; i--) {
+            System.out.println("nilai :"+i);
+
+        }
+    }
+}
