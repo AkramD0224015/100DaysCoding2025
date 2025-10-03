25d961ee13f860ec48d089ed06303a85d824ee74
Akram <akramsaja37@gmail.com>
2025-10-03 23:10:02 +0800

    day26.java
    Operator Penugasan (=, +=, -=, dll).

---
+++ day26.java
@@ -0,0 +1,19 @@
+package akram;
+
+public class day26 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        System.out.println("nilai awal: "+a);
+        a += 5;
+        System.out.println("a += 5: "+a);
+        a -= 3;
+        System.out.println("a -= 3: "+a);
+        a *= 2;
+        System.out.println("a *= 2: "+a);
+        a /= 4;
+        System.out.println("a /= 4: "+a);
+        a %= 5;
+        System.out.println("a %= 5: "+a);
+    }
+}
