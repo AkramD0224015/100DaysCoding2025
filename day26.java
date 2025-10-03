6527e2d80e95348e50db044fc0ced84ad4195a16
Akram <akramsaja37@gmail.com>
2025-10-03 23:20:56 +0800

    day26.java
    Operator Penugasan (=, +=, -=, dll).

--- day26.java
+++ day26.java
@@ -6,14 +6,14 @@ public class day26 {
         int a = 10;
         System.out.println("nilai awal: "+a);
         a += 5;
-        System.out.println("a += 5: "+a);
+        System.out.println("10 += 5: "+a);
         a -= 3;
-        System.out.println("a -= 3: "+a);
+        System.out.println("15 -= 3: "+a);
         a *= 2;
-        System.out.println("a *= 2: "+a);
+        System.out.println("12 *= 2: "+a);
         a /= 4;
-        System.out.println("a /= 4: "+a);
+        System.out.println("24 /= 4: "+a);
         a %= 5;
-        System.out.println("a %= 5: "+a);
+        System.out.println("6 %= 5: "+a);
     }
 }
