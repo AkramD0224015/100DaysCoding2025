dce81bc752d222c1e6de96c5544b93c8a3f9afca
Akram <akramsaja37@gmail.com>
2025-10-06 21:55:45 +0800

    day29.java
    Operator Perbandingan < dan >

---
+++ day29.java
@@ -0,0 +1,11 @@
+package akram;
+
+public class day29 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+
+        System.out.println("kurang dari :" + (a < b));//true
+        System.out.println("lebih dari  :" + (a > b));//false
+    }
+}
