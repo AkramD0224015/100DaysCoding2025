860dd7473c2f97cd31a642d883200ba968dffd5e
Akram <akramsaja37@gmail.com>
2025-10-08 17:50:41 +0800

    day31.java
    Operator Logika AND (&&).

---
+++ day31.java
@@ -0,0 +1,13 @@
+package akram;
+
+public class day31 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+
+        System.out.println(a < b && b < 25);//true
+        System.out.println(a > b && b < 25);//false
+        System.out.println(a == 10 && b == 15);//true
+
+    }
+}
