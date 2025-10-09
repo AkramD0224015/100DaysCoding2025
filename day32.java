557698716f6efa53d3cc41eb2867486099e6e83b
Akram <akramsaja37@gmail.com>
2025-10-09 21:30:06 +0800

    day32.java
    Operator Logika OR (||).

---
+++ day32.java
@@ -0,0 +1,12 @@
+package akram;
+
+public class day32 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+
+        System.out.println(a < b || a < 15);//true
+        System.out.println(a > b || a > 25);//false
+        System.out.println(a ==10|| b ==10);//true
+    }
+}
