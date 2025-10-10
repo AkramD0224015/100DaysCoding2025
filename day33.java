66bbd4ee5dbc79ba8696341fb5719143276f9ccd
Akram <akramsaja37@gmail.com>
2025-10-10 21:58:58 +0800

    day33.java
    Operator Logika NOT (!).

---
+++ day33.java
@@ -0,0 +1,10 @@
+package akram;
+
+public class day33 {
+        public static void main(String[] args) {
+            boolean hujan = false;
+
+            System.out.println("apakah hari ini hujan :"+!hujan);
+
+        }
+    }
