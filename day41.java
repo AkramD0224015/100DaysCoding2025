0dc11c13a8455b0ca7f018904e117d4124930772
Akram <akramsaja37@gmail.com>
2025-10-18 22:10:21 +0800

    day41.java
    Latihan: Menentukan Gaji Bersih

---
+++ day41.java
@@ -0,0 +1,23 @@
+package akram;
+import java.util.Scanner;
+public class day41 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== Menghitung Gaji Bersi ===");
+        System.out.print("Masukkan Gaji Pokok: ");
+        double gajiPokok = input.nextDouble();
+        System.out.print("Masukkan tunjangan Makan: ");
+        double tunjanganmakan = input.nextDouble();
+        System.out.print("Masukkan Potongan Koperasi: ");
+        double potonganKoperasi = input.nextDouble();
+
+        double gajiBersih = gajiPokok + tunjanganmakan - potonganKoperasi;
+
+        System.out.println("\n=== Hasil Perhitungan ===");
+        System.out.println("Gaji Pokok      : Rp"+gajiPokok);
+        System.out.println("tunjangan Makan : Rp"+tunjanganmakan);
+        System.out.println("Potongan Koperasi: Rp"+potonganKoperasi);
+        System.out.println("Gaji Bersih     : Rp"+gajiBersih);
+    }
+}
