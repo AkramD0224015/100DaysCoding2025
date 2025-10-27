191ebcdf4c52063445288274ff6ca6f4fa66d638
Akram <akramsaja37@gmail.com>
2025-10-28 00:49:42 +0800

    day51.java
    Perulangan for

---
+++ day51.java
@@ -0,0 +1,12 @@
+package akram;
+
+public class day51 {
+    public static void main(String[] args) {
+        System.out.println("menampilkan angka 1-5 ");
+
+        for (int i = 1; i <=5 ; i++) {
+            System.out.println("angka "+ i);
+
+        }
+    }
+}
