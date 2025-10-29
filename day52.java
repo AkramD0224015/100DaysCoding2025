2a8c7e1186c8a0a46e4089ea9ce7e34962fefce9
Akram <akramsaja37@gmail.com>
2025-10-29 21:23:24 +0800

    day52.java
    Perulangan while

---
+++ day52.java
@@ -0,0 +1,12 @@
+package akram;
+
+public class day52 {
+    public static void main(String[] args) {
+        System.out.println("menampilkan angka 1-5");
+        int angka = 1;
+        while (angka <= 5 ){
+            System.out.println("angka "+angka);
+            angka++;
+        }
+    }
+}
