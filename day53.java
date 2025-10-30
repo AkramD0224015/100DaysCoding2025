f13d7e1ca1c07a5eca033f156cc6051eec2eade3
Akram <akramsaja37@gmail.com>
2025-10-30 21:37:43 +0800

    day53.java
    Keyword break

---
+++ day53.java
@@ -0,0 +1,21 @@
+package akram;
+
+public class day53 {
+    public static void main(String[] args) {
+        int angka = 1;
+
+        switch (angka) {
+            case 1:
+                System.out.println("mie ayam");
+                break;
+            case 2:
+                System.out.println("soto");
+                break;
+            case 3:
+                System.out.println("bakso");
+                break;
+            default:
+                System.out.println("tidak valid");
+        }
+    }
+}
