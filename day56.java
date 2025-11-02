8c435ca6e7082be8025b07e120279f20644a77ef
Akram <akramsaja37@gmail.com>
2025-11-02 22:49:41 +0800

    day56.java
    Perulangan do-while

---
+++ day56.java
@@ -0,0 +1,12 @@
+package akram;
+
+public class day56 {
+    public static void main(String[] args) {
+        int a = 1;
+
+        do {
+            System.out.println("tampilkan " + a);
+            a++;
+        } while (a <= 5);
+    }
+}
