dee622258568e752241560b3193b6857c62fa0d6
Akram <akramsaja37@gmail.com>
2025-11-21 20:56:40 +0800

    day75.java
    Method String Dasar (length(), toUpperCase(), toLowerCase()).

---
+++ day75.java
@@ -0,0 +1,12 @@
+package akram;
+
+public class day75 {
+    public static void main(String[] args) {
+        String teks = "java itu menyenangkan ";
+
+        System.out.println("Teks awal            : "+teks);
+        System.out.println("Jumlah karakter      : "+teks.length());
+        System.out.println("SEMUA HURUF KAPITAL  : "+teks.toUpperCase());
+        System.out.println("Semua huruf kecil    : "+teks.toLowerCase());
+    }
+}
