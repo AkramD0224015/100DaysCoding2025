ade751a2063b838bed929f2fc03d5fb7703b4b0e
Akram <akramsaja37@gmail.com>
2025-11-27 22:15:14 +0800

    day81.java
    Latihan: Akses Elemen Array Dengan Indeks

---
+++ day81.java
@@ -0,0 +1,9 @@
+package akram;
+
+public class day81 {
+    public static void main(String[] args) {
+        int [] angka = {10, 20, 30, 40 , 50};
+        System.out.println("tampilkan : "+angka[4]);
+        }
+    }
+
