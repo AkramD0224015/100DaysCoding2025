fb93db68b48c78e2e8d7a2877efffaf9d1f2e8ff
Akram <akramsaja37@gmail.com>
2025-11-30 20:26:30 +0800

    day84.java
    evaluasi

---
+++ day84.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class day84 {
+    public static void main(String[] args) {
+        Scanner akr = new Scanner(System.in);
+        int N = akr.nextInt();
+
+        for (int i = 1; i <=N ; i++) {
+            System.out.println("iccang");
+
+        }
+
+    }
+  }
