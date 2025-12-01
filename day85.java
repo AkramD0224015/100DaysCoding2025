6a1c219e309cf58c6876ae0de7086719f36223d1
Akram <akramsaja37@gmail.com>
2025-12-01 20:36:38 +0800

    day85.java
    Latihan: Menentukan Angka Maksimal Array

---
+++ day85.java
@@ -0,0 +1,14 @@
+
+public class day85 {
+    public static void main(String[] args) {
+        int[] angka = { 65, 45, 99, 85, 21, 51};
+        int max = angka[0];
+
+        for (int i = 1; i < angka.length; i++) {
+            if (angka[i] > max) {
+                max = angka[i];
+            }
+        }
+        System.out.println("Angka terbesar adalah: "+ max);
+    }
+}
