9280a01cae756405fd323ce0cb22a11b6ca16af2
Akram <akramsaja37@gmail.com>
2025-12-05 22:30:16 +0800

    day89.java
    Method void

---
+++ day89.java
@@ -0,0 +1,10 @@
+package akram;
+
+public class day89 {
+    public static void tampilkanHalo() {
+        System.out.println("Halo, saya akram");
+    }
+    public static void main(String[] args) {
+        tampilkanHalo();
+    }
+}
