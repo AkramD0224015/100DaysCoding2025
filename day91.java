e59153bdd413cd4d12f9def5f0ce62c1d982c177
Akram <akramsaja37@gmail.com>
2025-12-07 21:42:18 +0800

    day91.java
    Parameter method

---
+++ day91.java
@@ -0,0 +1,11 @@
+package akram;
+
+public class day91 {
+    public static void halo(String nama) {
+        System.out.println("Halo " + nama);
+    }
+    public static void main(String[] args) {
+        halo("Akram");
+        halo("Freya");
+    }
+}
