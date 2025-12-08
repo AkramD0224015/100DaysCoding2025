4ba05a628675e03dff1f3e84f1c8842609891f2e
Akram <akramsaja37@gmail.com>
2025-12-08 22:53:27 +0800

    day92.java
    Keyword return

---
+++ day92.java
@@ -0,0 +1,11 @@
+package akram;
+
+public class day92 {
+    public static String halo(String nama) {
+        return "Halo " + nama;
+    }
+    public static void main(String[] args) {
+        String hasil = halo("Akram");
+        System.out.println(hasil);
+    }
+}
