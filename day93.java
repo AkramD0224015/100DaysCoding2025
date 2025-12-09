ec065dbe911cf8e2032232cacba9049c5d58b91f
Akram <akramsaja37@gmail.com>
2025-12-09 23:00:29 +0800

    day93.java
    Method ceil(), floor() dan round()

---
+++ day93.java
@@ -0,0 +1,18 @@
+package akram;
+
+public class day93 {
+    public static void main(String[] args) {
+        double a =4.5;
+
+        // Pembulatan Keatas
+        System.out.println("=== Contoh Math.ceil() ===");
+        System.out.println("ceil(4.5) = " + Math.ceil(a));
+        // Pembulatan Kebawah
+        System.out.println("\n=== Contoh Math.floor() ===");
+        System.out.println("floor(4.5) = " + Math.floor(a));
+        // Pembulatan Terdekat
+        System.out.println("\n=== Contoh Math.round() ===");
+        System.out.println("round(4.5) = " + Math.round(a));
+    }
+}
+
