f013fd4d714772890529eacde0a997a06b6dd2c2
Akram <akramsaja37@gmail.com>
2025-12-10 22:35:23 +0800

    day94.java
    Latihan: Mencetak dengan Method Void

---
+++ day94.java
@@ -0,0 +1,12 @@
+package akram;
+
+public class day94 {
+    public static void cetakPesan() {
+        System.out.println("aing mah capek");
+    }
+
+    public static void main(String[] args) {
+        cetakPesan();
+    }
+}
+
