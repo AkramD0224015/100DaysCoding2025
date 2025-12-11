26ac10f06ac25a728f6ee947864e31e168852d69
Akram <akramsaja37@gmail.com>
2025-12-11 21:38:40 +0800

    day95.java
    Latihan: Mencetak dengan Parameter Void

---
+++ day95.java
@@ -0,0 +1,10 @@
+package akram;
+
+public class day95 {
+    public static void cetakKata(String kata) {
+        System.out.println(kata);
+    }
+    public static void main(String[] args) {
+        cetakKata("belajar java");
+    }
+}
