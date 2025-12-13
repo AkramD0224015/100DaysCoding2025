c4d34c278bdcbaa3557588b411d9bd56a5756603
Akram <akramsaja37@gmail.com>
2025-12-13 22:11:53 +0800

    day97.java
    Latihan: Menghitung Luas Persegi dengan Method Non-void dan parameternya

---
+++ day97.java
@@ -0,0 +1,16 @@
+package akram;
+
+public class day97 {
+    static int hitungLuas(int sisi) {
+        return sisi * sisi;
+    }
+    public static void main(String[] args) {
+        int sisi = 6;
+        int luas = hitungLuas(sisi);
+
+        System.out.println("Sisi persegi = " + sisi);
+        System.out.println("Luas persegi = " + luas);
+    }
+}
+
+
